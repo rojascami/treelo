c6f65bc4363d68318dcda95be814430bd12262a2
Camila Rojas <68752568+rojascami@users.noreply.github.com>
2024-09-17 17:20:42 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/dessert-market",
 
   /**
    * Disable server-based image optimization. Next.js does not support
@@ -23,6 +23,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  
 };
 
 export default nextConfig;
